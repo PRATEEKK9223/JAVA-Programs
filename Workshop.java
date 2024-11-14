b46c848e709a9bd5a07272fdbfb12c4a28aa1102
PRATEEKK9223 <pratheekkpratheekk@gmail.com>
2024-11-14 09:33:56 +0530

    stash apply

--- Workshop.java
+++ Workshop.java
@@ -69,15 +69,28 @@ public class Workshop{
         } 
         return flag;  
     }
-
-    public static void main()
-    {
+    static void invoke_isDigit(){
         boolean result=isDigit("876521");
         if(result)
             System.out.println("yes it digit \n");
         else
             System.out.println("no it not digit\n");
+    }
+    //5.Function getgreeting by accepting name and print greeting with name?
 
+    public static void getGreeting(String name){
+        System.out.printf("hello,"+name);
+        System.out.println("\nwelcome, what you want to have\n");
+    }
+
+    public static void invoke_getGreeting(){
+        getGreeting("prateek k");
+        getGreeting("viraj");
+    }
+
+    public static void main()
+    {
+        invoke_getGreeting();
     }
     
     
