95b14b31598bf4428bec7fe7a21a813a5446343f
PRATEEKK9223 <pratheekkpratheekk@gmail.com>
2024-11-14 15:50:13 +0530

    added upto 8t problems

--- Workshop.java
+++ Workshop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Workshop{
 
     //  1.Function getsum to get sum of two numbers?
@@ -70,8 +72,7 @@ public class Workshop{
         return flag;  
     }
 
-    public static void main()
-    {
+    public static void invoke_isDigit(){
         boolean result=isDigit("876521");
         if(result)
             System.out.println("yes it digit \n");
@@ -79,7 +80,149 @@ public class Workshop{
             System.out.println("no it not digit\n");
 
     }
+
+    //5.Function getgreeting by accepting name and print greeting with name?
+
+    public static void getGreeting(String input){
+        System.out.println("hello, "+input);
+        System.out.printf("welcome %s",input);
+    }
+    static void invoke_getGreeting(){
+        Scanner reader= new Scanner(System.in);
+        String name;
+        name=reader.nextLine();
+        reader.close();
+        getGreeting((name)); 
+
+    }
+
+    //6.Functionto print ASCII values of strinf input?
+
+    static void printAscii(String name){
     
+        for(int i=0;i<name.length();i++)
+        {
+            char character=name.charAt(i);
+            int asciiValue=character;
+            System.out.println("Ascii value of char "+character+ " "+asciiValue);
+        }
+    }
+    static void invoke_printAscii(){
+        Scanner S =new Scanner(System.in); //this is for creating the object
+        String str=S.nextLine();//it is for read a string
+        S.close();//it compulsry to cloe the scanner object
+        printAscii(str);
+    }
+    // 7.Function getstrlength to get String to get string length?
+
+    public static int getLength(String str){
+        char[] array=str.toCharArray();
+        int length=0;
+        for(char x : array)//this is for each loop it traver each element of array
+        {
+            System.out.printf("%c ",x);
+            length++;
+        }
+        
+        return length;
+    }
+    static void invoke_getLength()
+    { 
+        Scanner scan = new Scanner(System.in);
+        String input;
+        input=scan.nextLine();
+        scan.close();
+        int length=getLength(input);
+        System.out.printf("\nthe length of the string %s is %d",input,length);
+    }
+
+    //8.Function getCOUNTOfOvewel to count ovewels in given string?
+     
+    public static int getCOUNTOfOvwels(String input)
+    {
+        int count=0;
+        char[] chArray=input.toCharArray();
+        for(char character : chArray)
+        {
+            character=toLower((character));
+            if(character=='a'|| character== 'e'
+            || character=='i'||character=='o'
+            ||character=='u')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static char toLower(char ch){
+        if(ch>='A' && ch <='Z') 
+        {
+            //int num=ch;
+            ch=(char) (ch+32);
+        }
+        return ch;
+    }
     
-   
+
+    static void invoke_getCOUNTOfOvwels(){
+        System.out.println("enter one string to count number of ovwels");
+       Scanner read =new Scanner(System.in);
+       String word;
+       word=read.nextLine();
+       read.close();
+       int result=getCOUNTOfOvwels(word);
+       System.out.println("te number of ovwels are "+ result);
+
+    }
+    //reversing  the str using another memory
+    static String reversStr(String str){
+        StringBuffer outstr=new StringBuffer();//here StringBuffer is the class that alows the modification in string
+        //char[] strArray=str.toCharArray();
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            outstr.append(str.charAt(i));
+        }
+        return outstr.toString();//to coner the buffer string to string
+    }
+
+    public static void invoke_reversStr(){
+        Scanner read=new Scanner(System.in);
+       System.out.printf("enter the str to revers\n");
+       String name,reversName;
+       name=read.nextLine();
+       read.close();
+       reversName=reversStr(name);
+       System.out.printf("reversed str="+reversName);
+    }
+
+    //rever the string in same memory>>>>
+
+    public static String Revers_v2(String input){
+        char[] arrayStr=input.toCharArray();
+        int start=0;
+        int end=input.length()-1;
+        while(start<end)
+        {
+            char temp=arrayStr[start];
+            arrayStr[start]=arrayStr[end];
+            arrayStr[end]=temp;
+            start++;
+            end--;
+        }
+        return new String(arrayStr);//to covert the array of str into str
+
+    }
+    public static void main()
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("enter the string to revers");
+        String name;
+        name = S.nextLine();
+        S.close();
+        System.out.println("BEFORE::"+name);
+        name=Revers_v2(name);
+        System.out.println("AFTER::"+name);
+
+    }
 }
